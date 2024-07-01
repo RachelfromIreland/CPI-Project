fe3270772aa2aff9b2a4fda34f5fd65671600c23
Rachel Mcglinchey <rachelfromireland@gmail.com>
2024-07-01 10:23:16 +0000

    Update book model to indclude fields

--- book/admin.py
+++ book/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Booking
 # Register your models here.
 
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('booking_name', 'num_attending', 'date', 'time_slot', 'booking_date')
+    list_filter = ('date', 'time_slot')
+    search_fields = ('booking_name')
+    ordering = ('date')
 
 admin.site.register(Booking)
