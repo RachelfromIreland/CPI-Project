7f40e2316b2031d846f6365e6cd12b0676cdd6cd
Rachel Mcglinchey <rachelfromireland@gmail.com>
2024-07-02 11:38:43 +0000

    Add static files to settings

--- cpi_ltd/settings.py
+++ cpi_ltd/settings.py
@@ -19,6 +19,10 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
