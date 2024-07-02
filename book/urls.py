36578294c0d9467b6ba9fbe8a85c7c822b70c185
Rachel Mcglinchey <rachelfromireland@gmail.com>
2024-07-02 18:14:11 +0000

    Add list and edit url patterns

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'book'
+
 urlpatterns = [
-    path('', views.book_party, name='book'),
+    path('', views.book_party, name='book_party'),
+    path('party_list/', views.party_list, name='party_list'),
+    path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
 ]
