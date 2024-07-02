5bef37246b236a6ccaf611c6449d57c7428a7ea6
Rachel Mcglinchey <rachelfromireland@gmail.com>
2024-07-02 18:15:44 +0000

    Change url path name to debug

--- cpi_ltd/urls.py
+++ cpi_ltd/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls'), name='home-urls'),
-    path('book/', include('book.urls'), name='book.urls'),
+    path('book/', include('book.urls'), name='book'),
 ]
