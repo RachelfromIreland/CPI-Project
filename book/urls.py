dc3f11e513b0e9f2a002a6a1a82a3a463292d565
Rachel Mcglinchey <rachelfromireland@gmail.com>
2024-07-03 13:22:25 +0000

    Add url pattern to delete bookings

--- book/urls.py
+++ book/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.book_party, name='book_party'),
     path('party_list/', views.party_list, name='party_list'),
     path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('delete_booking/<int:booking_id>/', views.delete_booking, name='delete_booking'),
 ]
