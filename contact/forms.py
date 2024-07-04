25449127476333f8d902bedbd382f8c4d3905c28
Rachel Mcglinchey <rachelfromireland@gmail.com>
2024-07-04 23:30:45 +0000

    Format to match pep8

--- contact/forms.py
+++ contact/forms.py
@@ -1,16 +1,17 @@
 from django import forms
 from django.core.validators import RegexValidator
 
+
 class ContactForm(forms.Form):
     CONTACT_METHOD_CHOICES = [
         ('email', 'Email'),
         ('phone', 'Phone'),
     ]
-    
+
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     phone_number = forms.CharField(
-        max_length=15, 
+        max_length=15,
         required=False,
         validators=[
             RegexValidator(
