bbc20884b21edf786b47af65f3e21a271a94d45b
Rachel Mcglinchey <rachelfromireland@gmail.com>
2024-07-04 23:29:38 +0000

    Edit format to match pep8

--- book/forms.py
+++ book/forms.py
@@ -3,6 +3,7 @@ from .models import Booking
 from django.utils import timezone
 from datetime import timedelta
 
+
 class BookingForm(forms.ModelForm):
     date = forms.DateField(
         widget=forms.DateInput(attrs={'type': 'date'}),
