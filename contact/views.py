4cb7cff5e82448b4b9a4e84713722bb938821078
Rachel Mcglinchey <rachelfromireland@gmail.com>
2024-07-04 12:28:13 +0000

    Add view for contact

--- contact/views.py
+++ contact/views.py
@@ -1,2 +1,13 @@
+from django.shortcuts import render
+from .forms import ContactForm
 
 # Create your views here.
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Process form data here
+            return render(request, 'contact/contact.html', {'form': form})
+    else:
+        form = ContactForm()
+    return render(request, 'contact/contact.html', {'form': form})
