76c377214d0b9b9f36de7ae772ce0f3e1aa9ba53
Rachel Mcglinchey <rachelfromireland@gmail.com>
2024-07-04 23:30:15 +0000

    Edit format to match pep8

--- book/urls.py
+++ book/urls.py
@@ -6,6 +6,10 @@ app_name = 'book'
 urlpatterns = [
     path('', views.book_party, name='book_party'),
     path('party_list/', views.party_list, name='party_list'),
-    path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
-    path('delete_booking/<int:booking_id>/', views.delete_booking, name='delete_booking'),
+    path(
+        'edit_booking/<int:booking_id>/', views.edit_booking, name='edit_'
+        'booking'),
+    path(
+        'delete_booking/<int:booking_id>/', views.delete_booking, name='delete'
+        '_booking'),
 ]
